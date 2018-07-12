f21d21e65cdd3579cd624f4a507f71fc50412fae
Alexnader Todorov <todorov@bezeqint.net>
2018-07-12 11:34:41 +0300

    Cleaner Thread need to fix exceptions

---
+++ src/threads/DailyCouponExpirationTask.java
@@ -0,0 +1,58 @@
+package threads;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.HashSet;
+
+import DAO.SqlTableUtil;
+import couponSystemException.CuponSystemException;
+import dbConnectionPool.ConnectionPool;
+
+public class DailyCouponExpirationTask implements Runnable{
+
+	boolean quit = false; 
+	public DailyCouponExpirationTask(boolean quit) {
+		super();
+		this.quit = quit;
+		
+	}
+	public boolean isQuit() {
+		return quit;
+	}
+	public void setQuit(boolean quit) {
+		this.quit = quit;
+	}
+	@Override
+	public void run() {
+		while (quit == false) {
+		Collection<Long> expiredCouponsIds = new HashSet<Long>();
+		ConnectionPool pool = ConnectionPool.getConnectionPool();
+		long time = System.currentTimeMillis();
+		Date date = new Date(time);
+		String sql = "SELECT ID FROM COUPON WHERE END_DATE > date ";
+		Connection con = pool.getConnection();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		while (rs.next()) {
+			expiredCouponsIds.add(rs.getLong("ID"));
+		}
+		for (long id : expiredCouponsIds) {
+		SqlTableUtil.removeWhere("COUPON", "ID", id);
+		SqlTableUtil.removeWhere("COMPANY_COUPON", "COUPON_ID", id);
+		SqlTableUtil.removeWhere("CUSTOMER_COUPON", "COUPON_ID", id);
+		}
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			try {
+				throw new CuponSystemException("Expiration Coupons cleaner failed" ,e );
+			} catch (CuponSystemException e1) {
+				
+			} 
+		}
+	}
+ }
+
+}
